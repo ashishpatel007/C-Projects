cf330aee053807fea28698ba4175b597c87b65c5
Ashish Patel <ashpat86@gmail.com>
2017-07-31 13:15:44 -0400

    Update with more programs
    Additional programs added.

---
+++ FavouriteSaying/FavouriteSaying/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Design a message display application. Allow users to enter their name and
+ * favorite saying in a single method that gets invoked two times. First call 
+ * the method asking for the person's name. Send a string argument indicating 
+ * what value should be entered. Invoke the method a second time to retreive the 
+ * favorite saying. Return the string values back to the Main() method. Call 
+ * another method, sending the name and saying. From the method, display the 
+ * message showing the person's name and their saying surrounded by rows of 
+ * greater than/lesss than symbols(<><><>).
+ */
+
+using System;
+
+namespace FavouriteSaying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Message("name");
+            string phrase = Message("favorite saying");
+
+            DisplayOutput(name, phrase);
+
+        }
+
+        public static string Message(string msg)
+        {
+            Console.WriteLine("Please enter your {0}: ", msg);
+            return Console.ReadLine();
+        }
+
+        public static void DisplayOutput(string name, string phrase)
+        {
+            Console.WriteLine("\n<><><><><><><><><><><><><><><><><><><><><><><><>"); 
+            Console.WriteLine("Your name is {0}", name);
+            Console.WriteLine("Your favorite saying is {0}", phrase);
+            Console.WriteLine("<><><><><><><><><><><><><><><><><><><><><><><><>\n");
+		}
+    }
+}
---
+++ StudentInformation/StudentInformation/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Create a class representing a student. Include characteristics such as student
+ * number, first and last name, overall GPA, classification, and major. Write at
+ * least two constructors. Include properties for each of the data items. Create a 
+ * second class that instantiates the first class with information about yourself. 
+ * In the second class, create a class method that displays your name and GPA.
+ */
+
+using System;
+
+namespace StudentInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
